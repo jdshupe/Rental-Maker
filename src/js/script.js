dae9da392b3b52b0f67074cc90098e96b2a1a070
john-shupe-amteck <jshupe@amteck.com>
2021-06-28 15:49:02 -0400

    Table selection button and documentation changes

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,9 @@
+/**
+ * Populates a select with values from the file db.json
+ *
+ * @param {string} element_id - the id of the select box to be populated
+ * @param {string} type - (rep/one_time) the type of information to populate
+ */
 function setup_select (element_id, type) {
   var dropdown = document.getElementById(element_id);
   dropdown.length = 0;
@@ -8,7 +14,8 @@ function setup_select (element_id, type) {
   dropdown.add(defaultOption);
   dropdown.selectedIndex = 0;
   
-  var url = `file:///resources/app/src/db.json`;
+  // var url = `file:///resources/app/src/db.json`;
+  var url = `file:///src/db.json`;
   
   var request = new XMLHttpRequest();
   request.open('GET', url, true);
@@ -47,6 +54,12 @@ setup_select('rep_charge_1','rep');
 setup_select('one_time_charge_1','one_time');
 
 
+/**
+ * Inserts a new Node (newNode) before another node (referenceNode)
+ *
+ * @param {object} newNode - Node to be added
+ * @param {object} referenceNode - Node to be inserted before
+ */
 function insertAfter(newNode, referenceNode) {
   referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
@@ -56,22 +69,30 @@ var added_rows_repeatable = 1;
 var added_rows_one_time = 1;
 
 
+/**
+ * Add a new line for rental information to the input form
+ *
+ * @param {*} chargeType
+ */
 function addRentalRow(chargeType) {
   var adder;
   var type;
   var description;
+  var indexHelper;
   switch (chargeType) {
     case 'repeatable':
       added_rows_repeatable++;
       adder = added_rows_repeatable;
       type = 'rep';
       description = 'Repeatable';
+      indexHelper = 1;
       break;
     case 'single':
       added_rows_one_time++;
       adder = added_rows_one_time;
       type = 'one_time';
       description = 'One Time';
+      indexHelper = 2;
       break;
     default:
       break;
@@ -86,10 +107,17 @@ function addRentalRow(chargeType) {
   code_select.id = `${type}_charge_${adder}`
 
   var price_entry = document.createElement('input')
-  price_entry.placeholder = 'Price'
   price_entry.classList.add(type,'price')
+  price_entry.placeholder = 'Price'
 
-  var cell0 = row.insertCell(0)
+  var deleteButton = document.createElement('button')
+  deleteButton.classList.add(type,'delete')
+  deleteButton.type = 'button'
+  deleteButton.innerHTML = 'X'
+  deleteButton.id = `${type}_charge_delete_${adder}`
+  deleteButton.setAttribute('onclick',`javascript: deleteRow('${row.id}');`) 
+
+  row.insertCell(0)
 
   var cell1 = row.insertCell(1)
   cell1.appendChild(code_select)
@@ -97,11 +125,20 @@ function addRentalRow(chargeType) {
   var cell2 = row.insertCell(2)
   cell2.appendChild(price_entry)
 
+  var cell3 = row.insertCell(3)
+  cell3.appendChild(deleteButton)
+
   insertAfter(row, document.getElementById(`${type}_charge_row_${adder - 1}`))
   setup_select(code_select.id, type)
 }
 
 
+/**
+ * Pulls a list of charges on the input form
+ *
+ * @param {*} type - (rep/one_time) what type of line to pull
+ * @return {object} contains the description code and price of each line
+ */
 function getLines (type) {
   var getLines = $(`tr.${type}`).map(function () {
     var select = $(this).find('select')
@@ -116,6 +153,10 @@ function getLines (type) {
 }
 
 
+/**
+ * Takes all info from the input form and creates the upload table, tied to button on html
+ *
+ */
 function createUpload () {
   $('#rental').toggle()
 
@@ -124,23 +165,31 @@ function createUpload () {
   var startDate = $('#start_date').val()  
   var duration = $('#duration').val()
   var rentalLength = parseInt($('input[name="rental length"]:checked').val());
+  var taxCode = $('#tax_code').val()
 
   items.repeatable.forEach(element => {
-    addLineToUpload(element,job_num,startDate,0,rentalLength,true)
+    addLineToUpload(element,job_num,startDate,0,rentalLength,taxCode,true)
   });
   items.oneTime.forEach(element => {
-    addLineToUpload(element,job_num,startDate,0,rentalLength)
+    addLineToUpload(element,job_num,startDate,0,rentalLength,taxCode)
   });
   if (duration > 1) {
     for (let month = 1; month < duration; month++) {
       items.repeatable.forEach(element => {
-        addLineToUpload(element,job_num,startDate,month,rentalLength,true)
+        addLineToUpload(element,job_num,startDate,month,rentalLength,taxCode,true)
       });
     }
   }
 }
 
 
+/**
+ * Pads a number with leading zeros
+ *
+ * @param {number} num - the number to be padded
+ * @param {number} size - the length of the number to be returned
+ * @return {string} - the initial number with enough leading zeros to be the size provided 
+ */
 function pad(num, size) {
   num = num.toString();
   while (num.length < size) num = "0" + num;
@@ -148,7 +197,17 @@ function pad(num, size) {
 }
 
 
-function addLineToUpload (dataObject, jobNumber, startDate, monthTick, rentalLength, repeatable) {
+/**
+ * Called by the create upload function to add the individual lines
+ *
+ * @param {object} dataObject
+ * @param {string} jobNumber
+ * @param {Date} startDate
+ * @param {number} monthTick
+ * @param {number} rentalLength
+ * @param {boolean} repeatable
+ */
+function addLineToUpload (dataObject, jobNumber, startDate, monthTick, rentalLength, taxCode, repeatable) {
   var row = document.createElement('tr')
   var descriptionWithDuration
 
@@ -188,8 +247,8 @@ function addLineToUpload (dataObject, jobNumber, startDate, monthTick, rentalLen
   per.innerHTML = 'EA'
   row.insertCell(11)
   row.insertCell(12)
-  var taxCode = row.insertCell(13)
-  taxCode.innerHTML = 'KY'
+  var taxCodeCell = row.insertCell(13)
+  taxCodeCell.innerHTML = taxCode
   row.insertCell(14)
   row.insertCell(15)
   var pol_chgovd = row.insertCell(16)
@@ -225,4 +284,31 @@ function addLineToUpload (dataObject, jobNumber, startDate, monthTick, rentalLen
   row.insertCell(39)
 
   document.getElementById('rental-body').appendChild(row)
+}
+
+function deleteRow(id) {
+  var row = document.getElementById(id)
+  row.remove()
+}
+
+
+function selectElementContents(el) {
+  var body = document.body, range, sel;
+  if (document.createRange && window.getSelection) {
+      range = document.createRange();
+      sel = window.getSelection();
+      sel.removeAllRanges();
+      try {
+          range.selectNodeContents(el);
+          sel.addRange(range);
+      } catch (e) {
+          range.selectNode(el);
+          sel.addRange(range);
+      }
+  } else if (body.createTextRange) {
+      range = body.createTextRange();
+      range.moveToElementText(el);
+      range.select();
+      document.execCommand("copy");
+  }
 }
