0cefbe68c61b38fbf705f5351d0fa115b5a94dee
john-shupe-amteck <jshupe@amteck.com>
2021-07-12 17:41:31 -0400

    selected value now loads into input

--- src/js/searchableSelect.js
+++ src/js/searchableSelect.js
@@ -7,11 +7,27 @@ function searchableSelect(input, repeatable) {
       for (let i = 0; i < mainData.Data.repeatable.length; i++) {
         var upper = mainData.Data.repeatable[i].Description.toUpperCase()
         if (upper.includes(text.toUpperCase())) {
-          var option = document.createElement('p')
+          var option = document.createElement('div')
           Object.assign(option, {
             innerHTML: mainData.Data.repeatable[i].Description,
-            className: 'search-option'
+            className: 'search-option',
+            id:        mainData.Data.repeatable[i].tsv_code
           })
+          option.setAttribute('onclick',`javascript: setValue(this);`);
+          input.nextElementSibling.append(option)
+        }
+      }
+    } else {
+      for (let i = 0; i < mainData.Data.oneTime.length; i++) {
+        var upper = mainData.Data.oneTime[i].Description.toUpperCase()
+        if (upper.includes(text.toUpperCase())) {
+          var option = document.createElement('div')
+          Object.assign(option, {
+            innerHTML: mainData.Data.oneTime[i].Description,
+            className: 'search-option',
+            id:        mainData.Data.oneTime[i].tsv_code
+          })
+          option.setAttribute('onclick',`javascript: setValue(this);`);
           input.nextElementSibling.append(option)
         }
       }
@@ -20,4 +36,11 @@ function searchableSelect(input, repeatable) {
   } else {
     $(div).hide();
   }
+}
+
+
+function setValue(node) {
+  var input = node.parentNode.previousElementSibling;
+  input.value = node.innerHTML;
+  $(node.parentNode).hide();
 }
